c97e61beafb5e37f415adceb3a69d07611a0e6e4
hotdogc1017 <hotdogc1017@gmail.com>
2024-08-12 14:30:28 +0800

    chore: 配置eslint以及编辑器编码格式

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,19 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-
-})
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+    // 需要明确启用, 否则部分规则(比如`max-len`)不生效:)
+    stylistic: true,
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 1,
+        multiline: 1,
+      }],
+    },
+  },
+)
